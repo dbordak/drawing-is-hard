40a91b6b4a107f3349db23a174dc36804bcfca3f
Revan Sopher <rsopher@gmail.com>
2016-01-23 04:33:56 -0500

    Landing page networking.

--- index.js
+++ index.js
@@ -2,10 +2,35 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname)); //TODO move static assets to a public directory
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var p2p = require('socket.io-p2p-server').Server;
+io.use(p2p);
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', function(socket){
+    console.log('a user connected at socket %s', socket.id);
+
+    socket.on('connAsPlayer', function(data) {
+        console.log('Connecting as player %s to room %s.', data.name, data.code);
+        socket.join(data.code);
+        io.to(data.code).emit('player-joined', {name: data.name, id: data.id});
+    });
+
+    socket.on('connAsMonitor', function(data) {
+        console.log('Connecting as monitor to room %s.', data.code);
+        socket.join(data.code);
+    });
+
+    socket.on('startGame', function(data) {
+        console.log('Starting Game!');
+        io.to(data.code).emit('startGame');
+    });
+
+});
+
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
--- js/landing.js
+++ js/landing.js
@@ -1,6 +1,34 @@
 var landing = {};
 
+landing.vm = (function() {
+    var vm = {};
+    vm.init = function() {
+        vm.code = m.prop("");
+        vm.name = m.prop("");
+
+        vm.connAsPlayer = function() {
+            console.log("connAsPlayer");
+            persistState.player = true;
+            persistState.code = vm.code();
+            persistState.name = vm.name();
+            socket.emit("connAsPlayer", {code: vm.code(), name: vm.name(), id: socket.id});
+            m.route('/lobby');
+        };
+
+        vm.connAsMonitor = function() {
+            console.log("connAsMonitor")
+            persistState.player = false;
+            persistState.code = vm.code();
+            socket.emit("connAsMonitor", {code: vm.code(), id: socket.id});
+            m.route('/tv/lobby');
+        };
+    }
+
+    return vm;
+}())
+
 landing.controller = function() {
+    landing.vm.init();
 };
 
 landing.view = function() {
@@ -12,14 +40,18 @@ landing.view = function() {
 			]),
 			m("div.eight columns", [
 				m("div.row", [
-					m("input", "Room Code")
+          m("div", "Room Code"),
+					m("input", {onchange: m.withAttr("value", landing.vm.code)}, "Room Code"),
+
+          m("div", "Player Name"),
+					m("input", {onchange: m.withAttr("value", landing.vm.name)}, "Player Name")
 				]),
 				m("div.row", [
 					m("div.six columns", [
-						m("button", "Connect as Player")
+						 m("button", {onclick: landing.vm.connAsPlayer}, "Connect as Player")
 					]),
 					m("div.six columns", [
-						m("button", "Connect as Monitor")
+						 m("button", {onclick: landing.vm.connAsMonitor}, "Connect as Monitor")
 					])
 				])
 			])
