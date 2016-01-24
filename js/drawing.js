48f1b81093b8c0a5f88ec14b280fbb75385c6c01
Daniel Bordak <dbordak@fastmail.fm>
2016-01-23 19:53:44 -0500

    Fix drawing

--- js/drawing.js
+++ js/drawing.js
@@ -5,13 +5,13 @@ drawing.controller = function() {
 
 drawing.view = function() {
 	return m("div.container", [
-		  m("div.row", [m("h1", "Draw: " + persistState.prompt.prompt)]),
-		m("div.row", [m("div.literally")]),
-		m("div.row", [m("button.u-full-width", {
+		m("h1", "Draw: " + persistState.prompt.prompt),
+		m("div.literally"),
+		m("button.u-full-width", {
 			onclick: function() {
 				console.log(lc.getSVGString());
 			}
-		}, "Submit")]),
+		}, "Submit"),
 		m("script", "var lc = LC.init(document.getElementsByClassName('literally')[0], {imageURLPrefix: '/img'});")
 	]);
 };
