2f3b594aac19257134a4a109a39897ca4b07df2a
Daniel Bordak <dbordak@fastmail.fm>
2016-01-24 05:13:09 -0500

    Added final vote for controller

--- js/proposals_artist.js
+++ js/proposals_artist.js
@@ -9,9 +9,9 @@ proposals_artist.view = function() {
 		m("h2", "look at it. look at and weep."),
 		m("h1#prompt-vote-title", "Rate this prompt?"),
 		m("div", [
-			m("button.button-row", "+"),
+			m("button.button-row.success", "+"),
 			m("button.button-row", "eh"),
-			m("button.button-row", "-")
+			m("button.button-row.error", "-")
 		])
 	]);
 };
